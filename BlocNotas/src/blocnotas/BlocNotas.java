edec590c3796c1d28c0451de35dd7b9dddf5c60d
Víctor Gallardo <victor.gallardo@iescampanillas.com>
2020-03-20 19:48:09 +0100

    Update BlocNotas.java

--- BlocNotas/src/blocnotas/BlocNotas.java
+++ BlocNotas/src/blocnotas/BlocNotas.java
@@ -9,12 +9,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-/**
- *
- *
- */
 public class BlocNotas {
-
   public static void main(String[] args) {
 
     Scanner s = new Scanner(System.in);
